fd891549a6cb58fe6c79ba631575cb5ce64f6166
Pavel Semkin <pvsemk@mail.ru>
2019-03-12 22:16:29 +0300

    Task 1 completed, turn on all tests

--- include/task1.h
+++ include/task1.h
@@ -10,7 +10,7 @@ bool compare(T a, T b)
 }
 
 template<>
-bool compare(char* a, char* b)
+bool compare<char*>(char* a, char* b)
 {
     return strlen(a) < strlen(b);
 }
@@ -24,17 +24,43 @@ void msort(T* array, size_t size)
     msort(&array[0], size / 2); // Sort the right half
     msort(&array[size/2], size - (size / 2)); // Sort the left half
 
-    T* resArr = new T[size]; // Result array for sorting
+    T* tmp = new T[size]; // Result array
 
-    size_t lhalf(0), rhalf(size / 2);
-    int idx(0);
+    size_t lidx(0), ridx(size / 2), tidx(0); // Index variables for left half, right half and temporary array
 
-    while ((lhalf < size / 2) || (rhalf < size))
+    while ((lidx < size / 2) || (ridx < size)) // Iterate over the whole array
     {
-        if ((lhalf < size / 2) && (rhalf < size))
+        if ((lidx < size / 2) && (ridx < size)) // Check if there still are number in both halves
         {
-            if
+            if (compare(array[lidx], array[ridx])) // The value from the left half is bigger
+            {
+                tmp[tidx++] = array[lidx];
+                lidx++;
+            }
+
+            else
+            {
+                tmp[tidx++] = array[ridx];
+                ridx++;
+            }
+        }
+
+        else if (lidx < size / 2) // There remain values only in the left half
+        {
+            tmp[tidx++] = array[lidx];
+            lidx++;
+        }
+
+        else
+        {
+            tmp[tidx++] = array[ridx];
+            ridx++;
         }
     }
+
+    for (size_t i(0); i < size; i++)
+        array[i] = tmp[i];
+
+    delete[] tmp;
 }
 #endif //TASK1_TASK1_H
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,7 +1,20 @@
 #include "task1.h"
 
+#include <iostream>
+
+
 int main()
 {
+    const size_t size = 10;
+    int array1[size] = {12, 6, -3, 0, 14, 12, 2, 17, 1, 15};
+    char* array2[size] = {"a", "abc", "aaaaa", "aaaa", "aaaaaaaaaa", "aaa", "aaaaaaa", "a", "", "aaaaaaaa"};
+    msort(array1, size);
+    msort(array2, size);
+    for (auto i(0); i < size; i++)
+        std::cout << array1[i] << ' ';
+    std::cout << std::endl;
+    for (auto i(0); i < size; i++)
+        std::cout << array2[i] << ' ';
     return 0;
 }
 
