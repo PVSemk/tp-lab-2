2e8d0599a955ffa7890740f91ad51f1ba49337ff
Pavel Semkin <pvsemk@mail.ru>
2019-03-12 21:28:40 +0300

    Task 1 started

--- include/task1.h
+++ include/task1.h
@@ -1,4 +1,40 @@
 #ifndef TASK1_TASK1_H
 #define TASK1_TASK1_H
 
+#include <cstring>
+
+template <typename T>
+bool compare(T a, T b)
+{
+    return a < b;
+}
+
+template<>
+bool compare(char* a, char* b)
+{
+    return strlen(a) < strlen(b);
+}
+
+template <typename T>
+void msort(T* array, size_t size)
+{
+    if (size < 2) // Stop the recursion
+        return;
+
+    msort(&array[0], size / 2); // Sort the right half
+    msort(&array[size/2], size - (size / 2)); // Sort the left half
+
+    T* resArr = new T[size]; // Result array for sorting
+
+    size_t lhalf(0), rhalf(size / 2);
+    int idx(0);
+
+    while ((lhalf < size / 2) || (rhalf < size))
+    {
+        if ((lhalf < size / 2) && (rhalf < size))
+        {
+            if
+        }
+    }
+}
 #endif //TASK1_TASK1_H
